c724db29427261f70d838d5f1c11eeb89e1d4017
Tobias Beese <tobias.beese@gmx.de>
2022-07-24 11:18:09 +0200

    Add "kill session" button to `SessionCard`

--- web/src/components/SessionCard/SessionCard.tsx
+++ web/src/components/SessionCard/SessionCard.tsx
@@ -1,12 +1,36 @@
+import { useMutation } from '@redwoodjs/web'
+
+import { QUERY as SessionsQuery } from 'src/components/SessionsCell'
+
 type SessionCardProps = {
   user: string
   busy: boolean
 }
 
+const KILL_SESSION_MUTUTAION = gql`
+  mutation KillCosminoSessionMutation($username: String!) {
+    killSession(username: $username)
+  }
+`
+
 const SessionCard = ({ user, busy }: SessionCardProps) => {
+  const [killSession] = useMutation(KILL_SESSION_MUTUTAION, {
+    variables: { username: user },
+    refetchQueries: [{ query: SessionsQuery }],
+  })
+
   return (
     <div className="h-20 rounded p-2 shadow-md">
-      <p className="text-xl">{user}</p>
+      <div className="flex items-baseline justify-between">
+        <p className="text-xl">{user}</p>
+        <button
+          title="Cosmino Sitzung Beenden"
+          className="ml-4 px-2 font-mono font-bold"
+          onClick={() => killSession()}
+        >
+          X
+        </button>
+      </div>
       <p>{busy && <span className="animate-pulse">buchung läuft</span>}</p>
     </div>
   )

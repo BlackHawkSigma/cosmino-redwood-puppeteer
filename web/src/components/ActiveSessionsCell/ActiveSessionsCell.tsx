a022cdcae281a6b3ded23beeaa1bc39b9b3fada9
Tobias Beese <tobias.beese@gmx.de>
2022-08-05 09:54:27 +0200

    Add spinner while busy

--- web/src/components/ActiveSessionsCell/ActiveSessionsCell.tsx
+++ web/src/components/ActiveSessionsCell/ActiveSessionsCell.tsx
@@ -58,7 +58,34 @@ export const Success = ({
                 <SessionCard {...session} />
               </div>
 
-              {session.src && <img src={session.src} alt="letze scannung" />}
+              {session.busy && (
+                <div className="flex h-[415px] w-[600px] items-center justify-center">
+                  <svg
+                    className="animate-spin-slow h-24 w-24 text-blue-700"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </div>
+              )}
+
+              {!session.busy && session.src && (
+                <img src={session.src} alt="letze scannung" />
+              )}
 
               <LastFiveLogsByUserCell username={session.user} key={terminal} />
             </div>

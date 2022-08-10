c400361efefcafe8d2d7ada80e4cb614ca81a3c7
Tobias Beese <tobias.beese@gmx.de>
2022-08-10 13:47:45 +0200

    Add `showSuccessCounter` to `User` model

---
+++ api/db/migrations/20220810100427_add_setting_for_successcounter_to_user/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME,
+    "showSuccessCounter" BOOLEAN NOT NULL DEFAULT true
+);
+INSERT INTO "new_User" ("hashedPassword", "id", "name", "password", "resetToken", "resetTokenExpiresAt", "salt") SELECT "hashedPassword", "id", "name", "password", "resetToken", "resetTokenExpiresAt", "salt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;

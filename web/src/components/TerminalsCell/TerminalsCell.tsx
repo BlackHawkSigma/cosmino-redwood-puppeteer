e7f614799e1815be544018e2ed562d6799b99c84
Tobias Beese <tobias.beese@gmx.de>
2022-08-31 14:51:48 +0200

    Add "logoff" button for claimed terminals

--- web/src/components/TerminalsCell/TerminalsCell.tsx
+++ web/src/components/TerminalsCell/TerminalsCell.tsx
@@ -4,7 +4,9 @@ import type { TerminalsQuery } from 'types/graphql'
 import { useLocalStorage } from 'usehooks-ts'
 
 import { Link, routes } from '@redwoodjs/router'
-import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { CellSuccessProps, CellFailureProps, useMutation } from '@redwoodjs/web'
+
+import { KILL_SESSION_MUTUTAION } from 'src/components/SessionCard'
 
 export const QUERY = gql`
   query TerminalsQuery {
@@ -27,8 +29,16 @@ export const Failure = ({ error }: CellFailureProps) => (
 )
 
 export const Success = ({ terminals }: CellSuccessProps<TerminalsQuery>) => {
+  const [killSession] = useMutation(KILL_SESSION_MUTUTAION, {
+    refetchQueries: [{ query: QUERY }],
+  })
+
   const [checked, setChecked] = useState(false)
   const [_terminal, setTerminal] = useLocalStorage('terminal', '')
+
+  const handleLogoff = (username) => {
+    killSession({ variables: { username } })
+  }
 
   return (
     <div className="p-4">
@@ -46,17 +56,23 @@ export const Success = ({ terminals }: CellSuccessProps<TerminalsQuery>) => {
 
       <div className="flex flex-col gap-4 p-2 ">
         {terminals.map(({ name: terminal, id, user }) => {
-          return (
+          return !user ? (
             <Link
               key={id}
               className="rounded border bg-slate-300 px-4 py-2 text-center text-xl"
               onClick={checked ? () => setTerminal(terminal) : null}
               to={routes.terminal({ terminal })}
             >
-              {user !== undefined
-                ? terminal
-                : `${terminal} belegt durch ${user?.name}`}
+              {terminal}
             </Link>
+          ) : (
+            <button
+              className="rounded border bg-slate-400 px-4 py-2 text-center text-xl"
+              onClick={() => handleLogoff(user.name)}
+            >
+              Terminal {terminal} belegt durch &quot;{user.name}&quot;.
+              abmelden?
+            </button>
           )
         })}
       </div>

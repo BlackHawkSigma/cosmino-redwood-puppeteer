e086fffabdc110e3b145b8c13ef9ac9b2a8a5908
Tobias Beese <tobias.beese@gmx.de>
2022-09-01 11:53:28 +0200

    Change not yet deployed migration
    Adds default values to `Terminal` model fields.

--- api/db/migrations/20220901094810_merge_session_model_into_terminal/migration.sql
+++ api/db/migrations/20220901094810_merge_session_model_into_terminal/migration.sql
@@ -2,8 +2,6 @@
   Warnings:
 
   - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
-  - Added the required column `busy` to the `Terminal` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `focused` to the `Terminal` table without a default value. This is not possible if the table is not empty.
   - Added the required column `id` to the `Terminal` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -21,8 +19,8 @@ CREATE TABLE "new_Terminal" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "userId" INTEGER,
-    "busy" BOOLEAN NOT NULL,
-    "focused" BOOLEAN NOT NULL,
+    "busy" BOOLEAN NOT NULL DEFAULT false,
+    "focused" BOOLEAN NOT NULL DEFAULT false,
     "lastSuccessImgUrl" TEXT,
     "loggedInAt" DATETIME,
     CONSTRAINT "Terminal_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE

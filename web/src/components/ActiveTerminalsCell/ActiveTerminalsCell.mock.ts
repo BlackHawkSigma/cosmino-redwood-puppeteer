497a0d8b30241ac3a4916c365064d8d0ba777d4e
Tobias Beese <tobias.beese@gmx.de>
2023-09-21 14:42:05 +0200

    Update mocks

--- web/src/components/ActiveTerminalsCell/ActiveTerminalsCell.mock.ts
+++ web/src/components/ActiveTerminalsCell/ActiveTerminalsCell.mock.ts
@@ -1,13 +1,29 @@
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
-  activeTerminals: [
+  dashboard: [
     {
-      id: 1,
-      name: '1',
-      user: { id: 1, name: 'John Doe' },
-      busy: false,
-      focused: true,
-      src: null,
+      terminal: {
+        id: 1,
+        name: '1',
+        user: {
+          id: 1,
+          name: 'John Doe',
+        },
+        busy: true,
+        focused: false,
+      },
+
+      logs: [
+        {
+          id: 1,
+          timestamp: new Date().toISOString(),
+          code: '123',
+          message: 'ok',
+          type: 'success',
+        },
+      ],
+
+      successCount: 1,
     },
   ],
   terminals: ['1', '2', '3'],

d28c300cf351e777ef244ada52aa21965db98b58
Tobias Beese <tobias.beese@gmx.de>
2023-10-12 10:59:37 +0200

    Deprecate "Pause" button and `refreshSession`

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { Link, routes } from '@redwoodjs/router'
 import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
 
 import { useAuth } from 'src/auth'
 import { KILL_SESSION_MUTUTAION } from 'src/components/SessionCard'
@@ -10,12 +11,6 @@ type MainLayoutProps = {
   children?: React.ReactNode
 }
 
-const REFRESH_SESSION_MUTATION = gql`
-  mutation RefreshCosminoSession($username: String!) {
-    refreshSession(username: $username)
-  }
-`
-
 const MainLayout = ({ children }: MainLayoutProps) => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
   const [killSession] = useMutation(KILL_SESSION_MUTUTAION, {
@@ -23,11 +18,6 @@ const MainLayout = ({ children }: MainLayoutProps) => {
     onCompleted: () => logOut(),
     onError: () => logOut(),
   })
-  const [refreshSession, { loading: isRefreshing }] = useMutation(
-    REFRESH_SESSION_MUTATION,
-    { variables: { username: currentUser?.name } }
-  )
-  const [isBreak, setIsBreak] = useState(false)
 
   useEffect(() => {
     const onUnload = () => killSession()
@@ -38,6 +28,12 @@ const MainLayout = ({ children }: MainLayoutProps) => {
       window.removeEventListener('beforeunload', onUnload)
     }
   }, [killSession])
+
+  const handlePauseButton = () => {
+    toast.success(
+      'Es ist nicht mehr notwending die Pause zu aktiviern.\nDieser Button wird demnächst entfernt'
+    )
+  }
 
   return (
     <>
@@ -67,16 +63,10 @@ const MainLayout = ({ children }: MainLayoutProps) => {
                 <li>
                   <button
                     type="button"
-                    onClick={() => {
-                      if (!isBreak) {
-                        refreshSession()
-                      }
-                      setIsBreak(!isBreak)
-                    }}
-                    disabled={isRefreshing}
+                    onClick={handlePauseButton}
                     className="rw-button text-xl disabled:pointer-events-none"
                   >
-                    Pause {isBreak ? 'Beenden' : 'Starten'}
+                    Pause Starten
                   </button>
                 </li>
               </>
@@ -85,16 +75,6 @@ const MainLayout = ({ children }: MainLayoutProps) => {
         </nav>
       </header>
       <main>{children}</main>
-      {isBreak && (
-        <div className="absolute top-0 bottom-0 left-0 right-0 bg-neutral-800/20 backdrop-blur-lg">
-          <button className="h-full w-full" onClick={() => setIsBreak(false)}>
-            <span className="font-comfortaa block p-8 text-8xl">Pause</span>
-            <span className="font-franklin text-6xl">
-              Bildschirm berühren zum fortfahren
-            </span>
-          </button>
-        </div>
-      )}
     </>
   )
 }
